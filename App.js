be4df8280eefbd84a6049a8945b0e85107585bec
Andrei Firsov <andrei.firsov@8base.com>
2019-01-30 16:57:24 +0300

    test

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
   View,
 } from 'react-native';
 import jwtDecoder from 'jwt-decode';
+import { AuthContext, AuthProvider } from '@8base/auth';
+import { ReactNativeAuth0AuthClient } from '@8base/react-native-auth0-auth-client';
 
 import gql from 'graphql-tag';
 import { ApolloClient } from 'apollo-client';
@@ -16,6 +18,9 @@ import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import { Query } from "react-apollo";
+
+//import { AuthProvider, AuthConsumer } from '@8base/auth';
+//import { ReactNativeAuth0AuthClient } from '@8base/react-native-auth0-auth-client';
 
 /*
   You need to swap out the Auth0 client id and domain with
@@ -27,9 +32,8 @@ import { Query } from "react-apollo";
   You can open this app in the Expo client and check your logs for "Redirect URL (add this to Auth0)"
   to see what URL to add if the above is confusing.
 */
-const auth0ClientId = 'qGHZVu5CxY5klivm28OPLjopvsYp0baD';
-const auth0Domain = 'https://8base.auth0.com';
-
+const auth0ClientId = 'VsfnxFXXF6bSHnmGat9uCXmavupN3ZOa';
+const auth0Domain = 'https://8base-dev.auth0.com';
 
 const httpLink = createHttpLink({
   uri: 'https://api.8base.com/cjnl27kn1000c01p6u3grcqsb',
@@ -70,68 +74,63 @@ function toQueryString(params) {
     .join('&');
 }
 
+const authClient = new ReactNativeAuth0AuthClient({
+  clientId: auth0ClientId,
+  domain: auth0Domain,
+});
+
+
 export default class App extends React.Component {
-  state = {
-    username: undefined,
+
+  renderAuthView = (auth) => {
+    console.log('auth', auth);
+
+    const login = async () => {
+      console.log('start login');
+
+      const authData = await auth.authorize();
+
+      console.log('authData', authData);
+
+      await auth.setAuthState({
+        token: authData.idToken,
+        email: authData.email,
+      });
+    };
+
+    const logout = async () => {
+      console.log('start logout');
+
+      await auth.purgeAuthState();
+    };
+
+    if (!auth.isAuthorized) {
+      return (
+        <View>
+          <Text style={styles.title}>Example: Auth0 login</Text>
+          <Button title="Login with Auth0" onPress={ login } />
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        <Text>{ auth.authState.email }</Text>
+        <Text style={styles.title}>Example: Auth0 logout</Text>
+        <Button title="Logout with Auth0" onPress={ logout } />
+      </View>
+    );
   };
-
-  _loginWithAuth0 = async () => {
-    const redirectUrl = AuthSession.getRedirectUrl();
-    console.log(`Redirect URL (add this to Auth0): ${redirectUrl}`);
-    const result = await AuthSession.startAsync({
-      authUrl: `${auth0Domain}/authorize` + toQueryString({
-        client_id: auth0ClientId,
-        response_type: 'id_token',
-        scope: 'openid profile',
-        redirect_uri: redirectUrl,
-        nonce: 'fakenonce'
-      }),
-    });
-
-    console.log(result);
-    if (result.type === 'success') {
-      this.handleParams(result.params);
-    }
-  }
-
-  handleParams = (responseObj) => {
-    console.log('responseObj:', responseObj);
-    if (responseObj.error) {
-      Alert.alert('Error', responseObj.error_description
-        || 'something went wrong while logging in');
-      return;
-    }
-    const encodedToken = responseObj.id_token;
-    const decodedToken = jwtDecoder(encodedToken);
-    const username = decodedToken.name;
-    console.log('decodedToken', decodedToken);
-    this.setState({ username });
-  }
 
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Query query={GET_PROPERTY}>
-          {({ loading, error, data }) => {
-            
-            if (loading) return <Text style={styles.title}>Loading...</Text>;
-            if (error) return <Text style={styles.title}>Error! {error.message}</Text>;
-
-            return (
-              <Text style={styles.title}>Test data: {data.property.title}</Text>
-            );
-          }}
-        </Query>
+      <AuthProvider authClient={ authClient }>
         <View style={styles.container}>
-          {this.state.username !== undefined ?
-            <Text style={styles.title}>Hi {this.state.username}!</Text> :
-            <View>
-              <Text style={styles.title}>Example: Auth0 login</Text>
-              <Button title="Login with Auth0" onPress={this._loginWithAuth0} />
-            </View>
-          }
+          <AuthContext.Consumer>
+            { this.renderAuthView }
+          </AuthContext.Consumer>
         </View>
-      </ApolloProvider>
+      </AuthProvider>
     );
   }
 }
